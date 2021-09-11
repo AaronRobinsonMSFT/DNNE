a763ee830242fd5dde4879c2cc436231276a0c88
Aaron Robinson <arobins@microsoft.com>
2021-09-10 19:00:10 -0700

    Add support for user defined compiler and linker flags.

--- src/msbuild/DNNE.BuildTasks/macOS.cs
+++ src/msbuild/DNNE.BuildTasks/macOS.cs
@@ -60,9 +60,20 @@ namespace DNNE.BuildTasks
             }
 
             compilerFlags.Append($"-o \"{Path.Combine(export.OutputPath, export.OutputName)}\" ");
+
+            if (!string.IsNullOrEmpty(export.UserDefinedCompilerFlags))
+            {
+                compilerFlags.Append($"{export.UserDefinedCompilerFlags} ");
+            }
+
             compilerFlags.Append($"\"{export.Source}\" \"{Path.Combine(export.PlatformPath, "platform.c")}\" ");
             compilerFlags.Append($"-lstdc++ ");
             compilerFlags.Append($"\"{Path.Combine(export.NetHostPath, "libnethost.a")}\" ");
+
+            if (!string.IsNullOrEmpty(export.UserDefinedLinkerFlags))
+            {
+                compilerFlags.Append($"{export.UserDefinedLinkerFlags} ");
+            }
 
             command = "clang";
             commandArguments = compilerFlags.ToString();
